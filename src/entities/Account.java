300b9e31961d3fa4066949385133a370db167cb3
Oygods <lucas.luca2ft@gmail.com>
2022-09-26 17:10:10 -0300

    feat: getters, setters

--- src/entities/Account.java
+++ src/entities/Account.java
@@ -11,9 +11,36 @@ public class Account {
         this.holder = holder;
     }
 
-    public Account(int number, String holder, double balance) {
+    public Account(int number, String holder, double initialDeposit) {
         this.number = number;
         this.holder = holder;
-        this.balance = balance;
+        deposit(initialDeposit);
     }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String Holder) {
+        this.holder = holder;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) {
+        balance -= amount + 5.0;
+    }
+
+
+
 }
